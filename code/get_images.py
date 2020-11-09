1949842b017c35d5ae41917ef8a868795a9693ba
Michelle Fridlund <michelle.fridlund@gmail.com>
2020-11-09 11:53:25 +0100

    Display and save .npy as .png figures

--- code/get_images.py
+++ code/get_images.py
@@ -1,25 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 27 14:05:49 2020
+Created on Wed Nov 27 11:44:33 2019
 
 @author: michellef
 """
 import os
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt                                                              
+import glob
+import numpy as np 
+#from PIL import Image
+import pydicom
+from pathlib import Path
+import dicom, dicom.UID
+from dicom.dataset import Dataset, FileDataset
 
 
+def check_dir(output):
+    if not os.path.exists(output):
+        os.makedirs(output)
+    return None
 
-def load_data(my_np):
-    my_ims = np.load(my_np)
-    return im_conv = np.stack(my_ims, axis=2).astype("uint8")
+# def directory_check(dir_path):
+#     if str(os.getcwd()) != str(dir_path):
+#         os.chdir(dir_path)  
+#     return None
 
-def get_image(my_np):
-    my_np = load_data(my_np)
-    return plt.imshow(my_np[:,:,0])
+def find_dirs(dir_path):
+    numpys = {}
+    for (dirpath, dirnames, filenames) in os.walk(dir_path):
+        dirname = str(Path(dirpath).relative_to(dir_path))
+        for n in glob.glob("{}/*.npy".format(dirpath), recursive=True):
+            new_name = str(Path(os.path.basename(n)).with_suffix(''))
+            numpys[n] = {'Dir': dirname,'Name': new_name}
+    return numpys
 
-file = '/homes/michellef/Amyloid/data_michelle/adaxmvprdo/Fulldose_SUV.npy'
+def load_data(dir_path, output):
+    numpys = find_dirs(dir_path)
+    for k,v in numpys.items():
+        np_pixels = np.load(k)
+        save_dir = os.path.join(str(output), str(v['Dir']))
+        check_dir(save_dir)
+        plt.imshow(np_pixels[:,:,0])
+        plt.savefig(os.path.join(save_dir, str(v['Name'] + '.png')))
 
-get_image(file)
+        
+def main():
+    dir_path = Path('/homes/michellef/Amyloid/data_michelle')
+    output = Path('/homes/michellef/Amyloid/IMAGES')
+    load_data(dir_path, output)
 
+    
+
+if __name__ == '__main__':
+    main()
+    
